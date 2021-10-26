3d57f47f698808e53361aad18aad880abf813e43
Emmanuel Worgu <emmanuelworgu@gmail.com>
2021-10-26 18:33:59 +0100

    started working

---
+++ functions/walletGen.js
@@ -0,0 +1,17 @@
+// program to generate wallet address
+
+// declare all characters
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function walletGen() {
+    let result = ' ';
+    const length = 34
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+module.exports = walletGen;
